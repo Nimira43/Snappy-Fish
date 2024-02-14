40ab80b8b64a76ef4b21e645deb409c2e1029a9b
Nick Rabone <n.rabone72@aol.com>
2024-02-14 02:35:55 +0000

    3rd - add gravity to player

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ class Game {
         this.baseHeight = 720
         this.ratio = this.height / this.baseHeight
         this.player = new Player(this)
+        this.gravity;
 
         this.resize(window.innerWidth, window.innerHeight)
         
@@ -21,6 +22,7 @@ class Game {
         this.width = this.canvas.width
         this.height = this.canvas.height
         this.ratio = this.height / this.baseHeight
+        this.gravity = 0.15 * this.ratio
         this.player.resize()
     }
 
